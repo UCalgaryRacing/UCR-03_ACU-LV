e4f27fc987de1f012819e5dc42552ec28a28ce92
Ethan Sallis <ethan.sallis@ucalgary.ca>
2026-06-12 02:31:47 -0600

    comp spec

--- Core/shared/service/rco/rco_svc_can_route.c
+++ Core/shared/service/rco/rco_svc_can_route.c
@@ -16,16 +16,16 @@ status_t rco_svc_can_route_init(void)
 
     FDCAN_FilterTypeDef sFilterConfig;
 
-    sFilterConfig.IdType = FDCAN_STANDARD_ID;
-    sFilterConfig.FilterIndex = 0;
-    sFilterConfig.FilterType = FDCAN_FILTER_MASK;
-    sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
-    sFilterConfig.FilterID1 = 405;
-    sFilterConfig.FilterID2 = 0x7FF;
-    sFilterConfig.RxBufferIndex = 0;
-    HAL_FDCAN_ConfigFilter(&hfdcan2, &sFilterConfig);
+    // sFilterConfig.IdType = FDCAN_STANDARD_ID;
+    // sFilterConfig.FilterIndex = 0;
+    // sFilterConfig.FilterType = FDCAN_FILTER_MASK;
+    // sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
+    // sFilterConfig.FilterID1 = 405;
+    // sFilterConfig.FilterID2 = 0x7FF;
+    // sFilterConfig.RxBufferIndex = 0;
+    // HAL_FDCAN_ConfigFilter(&hfdcan2, &sFilterConfig);
 
-    HAL_FDCAN_ConfigGlobalFilter(&hfdcan2, FDCAN_REJECT, FDCAN_REJECT, FDCAN_REJECT_REMOTE, FDCAN_REJECT_REMOTE);
+    // HAL_FDCAN_ConfigGlobalFilter(&hfdcan2, FDCAN_REJECT, FDCAN_REJECT, FDCAN_REJECT_REMOTE, FDCAN_REJECT_REMOTE);
 
     g_initialized = true;
     return OK;
